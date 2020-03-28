870afa3531d8bf5686acd2eab98a07a0df365b57
Cédric Bühler <cedric.buehler@gdels.com>
2020-03-28 22:29:59 +0100

    Make the function name unique

--- backend/apps/role/models.py
+++ backend/apps/role/models.py
@@ -6,7 +6,7 @@ from django.db import models
 
 class Function(models.Model):
 
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
 
 
 class Role(models.Model):
