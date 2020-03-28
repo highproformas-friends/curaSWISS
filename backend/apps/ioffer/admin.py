58ae4043f360407b3bc58f3cdcd48e254012dc4d
Cédric Bühler <cedric.buehler@gdels.com>
2020-03-28 21:29:36 +0100

    Enable Admin UI for ioffer and ineed

--- backend/apps/ioffer/admin.py
+++ backend/apps/ioffer/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import IOffer
 
 # Register your models here.
+
+admin.site.register(IOffer)
--- backend/apps/role/urls.py
+++ backend/apps/role/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    path('$/', views.roles, name='roles'),
-    path('$/', views.functions, name='functions')
+
 ]
