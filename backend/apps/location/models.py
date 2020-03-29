2745bead9dc3a5bbcb5f1c24bdc7f50eae372a98
Amin <amintrabi@hotmail.com>
2020-03-29 15:39:41 +0200

    added __str__ overload to zipcode and countrycode to change the default display text

--- backend/apps/location/models.py
+++ backend/apps/location/models.py
@@ -8,6 +8,9 @@ class CountryCode(models.Model):
     code_alpha_2 = models.CharField(max_length=2, unique=True)
 
 
+    def __str__(self):
+        return self.code_alpha_2
+
 class ZipCode(models.Model):
 
     country_code = models.ForeignKey(CountryCode, on_delete=models.CASCADE)
@@ -16,4 +19,7 @@ class ZipCode(models.Model):
     lat = models.DecimalField(max_digits=11, decimal_places=8)
     lng = models.DecimalField(max_digits=11, decimal_places=8)
     locality = models.CharField(max_length=50, null=False)
+
+    def __str__(self):
+        return self.locality
 
