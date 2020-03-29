5468123ac5084f32bff68c549205dff0decbc8ab
Niclas Simmler <niclas.simmler@gmail.com>
2020-03-29 16:03:16 +0200

    implemented map and filtering basic

--- backend/apps/map/views.py
+++ backend/apps/map/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from apps.ioffer.models import IOffer
+
 def default_map(request):
     mapbox_access_token = 'pk.eyJ1IjoibnNpaWNtMCIsImEiOiJjazhjNDJkd2cwaW5iM2twaTM4ZXZrcm10In0.efXuicWK6cjTrJJjWp1jQA'
     functions, offers = fetch_functions()
+    coordinates = map_postalcodes_with_coordinates(offers)
     return render(request, 'map.html',
                   {'mapbox_access_token': mapbox_access_token,
-                   'functions':functions, 'offers': offers})
+                   'functions':functions,
+                   'offers': offers,
+                   'coordinates': coordinates})
 
 def fetch_functions():
     ioffers = IOffer.objects.all() #here we could do some filtering
@@ -13,14 +17,22 @@ def fetch_functions():
     functions = list()
     for ioffer in ioffers:
         plz = ioffer.plz
-        role = ioffer.role
-        funcs = [f.name for f in role.functions.all()]
+        funcs = [f.name for f in ioffer.offer_functions.all()]
         functions.extend(funcs)
         func_counts = dict()
+
         for f in set(funcs):
             func_counts[f] = funcs.count(f)
+
         if plz not in offered_functions:
             offered_functions[plz] = func_counts
         else:
             offered_functions[plz] = (offered_functions[plz]).update(func_counts)
+
     return list(set(functions)), offered_functions
+
+def map_postalcodes_with_coordinates(offers):
+    return dict({
+                '8050': {'lat':47.4047172, 'lng': 8.5046483},
+                '8048': {'lat':47.3953996, 'lng': 8.4618351}
+                 })
