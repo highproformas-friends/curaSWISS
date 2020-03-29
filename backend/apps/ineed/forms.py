28672946acaefcee1fd37f82e10a5636a378c56b
Dominik Vazquez <dominikvazquez@web.de>
2020-03-29 20:05:41 +0200

    Changed to new Role, Function Changed form arrangement

--- backend/apps/ineed/forms.py
+++ backend/apps/ineed/forms.py
@@ -1,12 +1,13 @@
 from django.forms import *
-from apps.ineedstudent.models import Hospital
 from django.db import models
 from django import forms
+from django_select2.forms import Select2Widget, ModelSelect2Widget
 
 from django.utils.translation import gettext_lazy as _
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Fieldset, ButtonHolder, Submit, HTML, Row, Column, Div
 from apps.ineed.models import INeed
+from apps.location.models import CountryCode, ZipCode
 
 class INeedForm(ModelForm):
 
@@ -38,13 +39,39 @@ class INeedForm(ModelForm):
         self.helper.form_action = 'submit_survey'
 
         self.helper.layout = Layout(
-                Row(Column('organization_name', css_class='TEST') , Column('responsible_person')),
+                Row(Column('organization_name') , Column('responsible_person')),
                 Row(Column('appears_in_map')),
                 Row(Column('phone_number'), Column('email')),
-                Row(Column('zip_code'), Column('countrycode')),
-                'other_information'
+                Row(Column('countrycode'), Column('location')),
+                Row(Column('other_information'))
         )
 
-        # Moved from inherited class
-        self.helper.add_input(Submit('submit', 'Jetzt registrieren',onclick="this.form.submit(); this.disabled=true; this.value='Sending…';"))
+        self.helper.layout.extend((
+            HTML('<hr style="margin-top: 20px; margin-bottom:30px;">'),
+            HTML('<p class="text-left">'),
+            'datenschutz_zugestimmt',
+            HTML("</p>"),
+            HTML('<p class="text-left">'),
+            'einwilligung_datenweitergabe',
+            HTML("</p>"),
+            HTML('<div class="registration_disclaimer">{}</div>'.format(_('Die Bereitstellung unseres Services erfolgt unentgeltlich. Mir ist bewusst, dass die Ausgestaltung des Verhältnisses zur Institution allein mich und die entsprechende Institution betrifft. Insbesondere Art und Umfang der Arbeit, eine etwaige Vergütung und vergleichbares betreffen nur mich und die entsprechende Institution. Eine Haftung von curaSWISS ist ausgeschlossen.'))),
+            Submit('submit', _('Jetzt registrieren'), css_class='btn blue text-white btn-md',
+                   onclick="this.form.submit(); this.disabled=true; this.value='Sending…';"),
+        ))
 
+
+class INeedFormAndMail(INeedForm):
+    email = forms.EmailField()
+    countrycode = forms.ModelChoiceField(queryset=CountryCode.objects.all().order_by('code_alpha_2'),
+                                         widget=Select2Widget)
+
+    location = forms.ModelChoiceField(
+        queryset=ZipCode.objects.all(),
+        label=u"Location",
+        widget=ModelSelect2Widget(
+            model=ZipCode,
+            search_fields=['plz__icontains', 'locality__icontains'],
+            dependent_fields={'countrycode': 'country_code'},
+            max_results=500,
+        )
+    )
--- backend/apps/ineed/models.py
+++ backend/apps/ineed/models.py
@@ -5,6 +5,7 @@ import uuid
 from datetime import datetime
 from apps.role.models import Role, Function
 from apps.location.models import ZipCode
+from apps.skill.models import Skill
 
 # Create your models here.
 
@@ -12,45 +13,25 @@ from apps.location.models import ZipCode
 class INeed(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    role = models.ForeignKey(Role, on_delete=models.CASCADE, null=True)
 
+    role = models.ForeignKey(Role, on_delete=models.CASCADE, null=True)
     needed_functions = models.ManyToManyField(Function)
+    needed_skills = models.ManyToManyField(Skill)
 
     location = models.ForeignKey(ZipCode, on_delete=models.CASCADE, null=True)
-
     organization_name = models.CharField(max_length=50, default='')
     phone_number = models.CharField(max_length=100,default='')
-    zip_code = models.CharField(max_length=5, null=True)
+    #zip_code = models.CharField(max_length=5, null=True)
     other_information = models.TextField(default='')
     responsible_person = models.CharField(max_length=100,default='')
-    email = models.EmailField(unique=True)
+    #email = models.EmailField(unique=True)
 
-    uuid = models.CharField(max_length=100, blank=True, unique=True, default=uuid.uuid4)
+    uuid = models.CharField(max_length=100, blank=True, unique=True, default=uuid.uuid4, editable=False)
     registration_date = models.DateTimeField(default=datetime.now, blank=True, null=True)
     is_approved = models.BooleanField(default=False)
     appears_in_map = models.BooleanField(default=True)
     
-    # Hard coded... :-( Multiple values for later use
-    COUNTRY_CODE_CHOICES = [
-        # ("DE", 'Deutschland'),
-        # ("AT", 'Österreich'),
-        ("CH", 'Schweiz'),
-    ]
-    countrycode = models.CharField(
-        max_length=2,
-        choices=COUNTRY_CODE_CHOICES,
-        default="CH",
-    )
-
-    class Meta:
-        ordering = ['zip_code']
-
     def __str__(self):
         """String for representing the MyModelName object (in Admin site etc.)."""
-        return self.uuid
-
-    # def clean(self):
-    #     if self.plz not in plzs[self.countrycode]:
-    #         raise ValidationError(_(str(self.plz) + " ist keine Postleitzahl in " + self.countrycode))
-
+        return self.user.email
 
